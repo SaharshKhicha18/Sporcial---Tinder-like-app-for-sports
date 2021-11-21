512dc80c46deb8aa5de63ece900c4bd31efbe267
Aashana Shah <ashah@connect.hku.hk>
2021-11-21 23:51:43 +0800

    joinScreen

--- screens/joinScreen.js
+++ screens/joinScreen.js
@@ -1,32 +1,123 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Button, Text, TextInput, View, StyleSheet, Image} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Text,
+  View,
+  SafeAreaView, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const JoinScreen = ({ navigation, route }) => {
-    return (
-        <View>
-         <Text>
-           Here we will have the existing events!
-         </Text>
-        </View>
-    );
-  };
 
-  const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderColor: '#FFB3B3',
-      backgroundColor: '#FFB3B3',
-      borderWidth: 1,
-      padding: 10,
-    },
-    butt: {
-      backgroundColor: '#FFB3B3',
-      height: 80,
-    },
+import Carousel from 'react-native-snap-carousel';
+
+
+export default class App extends React.Component {
+  lastTap = null;
+  styles = StyleSheet.create({
+    header: {
+      backgroundColor: 'red',
+      height: hp('9%'),
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center'
+  },
+  arrow: {
+      color: 'white',
+      fontSize: hp('4%'),
+      marginHorizontal: wp('3%')
+  }
   });
+  IMAGES = [
+    require("./images/3.jpg"),
+    require("./images/2.jpg"),
+    require("./images/4.jpg"),
+    require("./images/5.jpg"),
+    require("./images/1.jpg"),
+  ];
 
-  export default JoinScreen;
+    constructor(props){
+        super(props);
+        this.state = {
+          activeIndex:0,
+          carouselItems: [
+          {
+              title:"Item 1",
+              text: "Text 1",
+              image: this.IMAGES[0]
+          },
+          {
+              title:"Item 2",
+              text: "Text 2",
+              image: this.IMAGES[1]
+          },
+          {
+              title:"Item 3",
+              text: "Text 3",
+              image: this.IMAGES[2]
+          },
+          {
+              title:"Item 4",
+              text: "Text 4",
+              image: this.IMAGES[3]
+          },
+          {
+              title:"Item 5",
+              text: "Text 5",
+              image: this.IMAGES[4]
+          },
+        ]
+      }
+    }
+    handleDoubleTap=()=>{
+      const now = Date.now();
+      const DOUBLE_PRESS_DELAY = 300;
+      if (this.lastTap && (now - this.lastTap) < DOUBLE_PRESS_DELAY) {
+        this.lastTap = null;
+        this.log();
+      } else {
+        this.lastTap = now;
+      }
+    }
+    log(){
+      console.log("SUCCESS");
+    }
+    _renderItem = ({item,index}) => {
+      return (
+        <TouchableOpacity onPress={this.handleDoubleTap}>
+        <View style={{
+            backgroundColor:'#fff0f0',
+            borderRadius: 5,
+            height: hp('80%'),
+            marginLeft: 25,
+            marginRight: 25}}>
+              <Image style={{ width: '100%', height: '70%' }}
+              source={item.image}/>
+          <Text style={{fontSize: 30, color: 'black'}}>{item.title}</Text>
+          <Text style={{color:'black'}}>{item.text}</Text>
+        </View>
+        </TouchableOpacity>
+      )
+  }
+
+    render() {
+        return (
+          <SafeAreaView style={{flex: 1, backgroundColor:'white'}}>
+            <View style={this.styles.header}>
+                <TouchableOpacity onPress = {() => navigation.navigate('homescreen')}>
+                <Icon name= 'arrow-left' style = {this.styles.arrow} />
+                </TouchableOpacity>
+                <Image source={require('./images/logo.png')} />
+        </View>
+            <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', paddingTop: 50 }}>
+                <Carousel
+                  layout={"default"}
+                  ref={ref => this.carousel = ref}
+                  data={this.state.carouselItems}
+                  sliderWidth={200}
+                  itemWidth={410}
+                  renderItem={this._renderItem}
+                  onSnapToItem = { index => this.setState({activeIndex:index}) } />
+            </View>
+          </SafeAreaView>
+        );
+    }
+}
