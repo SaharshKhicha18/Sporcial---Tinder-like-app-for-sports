4d463df64072cb464705ee64914ab45991aee0ed
Saharsh Khicha <71884962+SaharshKhicha18@users.noreply.github.com>
2021-11-24 20:38:33 +0800

    combine home and host

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -2,9 +2,12 @@ import * as React from 'react';
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Button, Text, TextInput, View, StyleSheet, Image} from 'react-native';
+import {Button, Text, TextInput, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const HomeScreen = ({ navigation, route}) => {
 
@@ -20,13 +23,39 @@ const HomeScreen = ({ navigation, route}) => {
   let buttonTxt = 'Welcome to Sporcial, ' + name;
    
     return (
-      <Button 
-        title ={buttonTxt}
-        onPress={() =>
-          navigation.navigate('hostscreen', {userID: id, username: name})
-        }
-        color = 'red'
-      />
+      <View style ={styles.container}>
+        <View style={styles.header}>
+                <TouchableOpacity onPress = {() => navigation.navigate('loginScreen')}>
+                <Icon name= 'arrow-left' style = {styles.arrow} />
+                </TouchableOpacity>
+                <Image source={require('./images/logo.png')} />
+        </View>
+        <Text> {{buttonTxt}} </Text>
+        <SafeAreaView >
+        <TouchableOpacity
+         onPress = {()=>
+          navigation.navigate('eventform')
+      }
+         >
+            <Image style={styles.imagestyle}
+            source={require('./images/hostImage.png')} />
+
+               
+        </TouchableOpacity>
+        </SafeAreaView>
+
+        <SafeAreaView >
+         <TouchableOpacity
+         onPress = {()=>
+          navigation.navigate('joinscreen')
+      }
+         >
+            <Image style={styles.imagestyle}
+            source={require('./images/joinImage.png')} />
+               
+        </TouchableOpacity>
+        </SafeAreaView>
+      </View>
     );
   };
 
@@ -39,10 +68,28 @@ const HomeScreen = ({ navigation, route}) => {
       borderWidth: 1,
       padding: 10,
     },
-    butt: {
-      backgroundColor: '#FFB3B3',
-      height: 80,
-    },
+    container: {
+      flex: 1,
+      alignItems: 'center'
+
+  },
+  imagestyle: {
+    justifyContent: 'center',
+    height: 200,
+    width: 200
+  },
+  arrow: {
+    color: 'white',
+    fontSize: hp('4%'),
+    marginHorizontal: wp('3%')
+  },
+  header: {
+    backgroundColor: 'red',
+    height: hp('9%'),
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center'
+},
   });
 
   export default HomeScreen;
